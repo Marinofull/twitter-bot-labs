675d574408dfa44a652d4c655d59e43dee3640b2
Marinofull <Marinofull@users.noreply.github.com>
2017-05-06 05:38:43 -0300

    funcionando

--- app/bot.py
+++ app/bot.py
@@ -1,15 +1,31 @@
 import tweepy
-from secrets import *
+#import requests
+from configs import *
 
-#create an OAuthHandler instance
-# Twitter requires all requests to use OAuth for authentication
+class BotStreamer(tweepy.StreamListener):
+
+    def on_status(self, status):
+        username = status.user.screen_name
+        status_id = status.id
+
+        tweet_echo(username, status_id)
+
+
+def tweet_echo(username, status_id):
+    #request = requests.get(url, stream=True)
+    #if request.status_code == 200:
+    api.update_status(status='hi @{0}'.format(username) , in_reply_to_status_id=status_id)
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
 auth.set_access_token(access_token, access_secret)
-
-#Construct the API instance
 api = tweepy.API(auth) # create an API object
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+myStreamListener = BotStreamer()
+stream = tweepy.Stream(auth, myStreamListener)
+
+stream.filter(track=[appname])
+
+
+#user = api.get_user('tname')
+#for friend in user.friends():
+#    print(friend.screen_name)
